a22e42ec2cb1db0062e09668221b392b163d1db0
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-04-21 00:27:19 -0400

    Added ProductCard component

---
+++ store/src/components/ProductCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ProductCard(props) {
+  //props.product is the prod we are selling
+  return <div>ProductCard</div>;
+}
+
+export default ProductCard;
--- store/src/pages/Store.js
+++ store/src/pages/Store.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { productsArray } from "../ProductsStore";
 
 function Store() {
-  return <h1>Welcome to the store</h1>;
+  return (
+    <>
+      <h1 align="center" className="p-3">
+        Welcome to the store
+      </h1>
+      <Row xs={1} md={3} className="g-4">
+        {productsArray.map((product, idx) => (
+          <Col align="center" key={idx}>
+            <h1>{product.title}</h1>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Store;
