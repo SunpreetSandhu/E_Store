254f0f16ae507fb074921cd0ad229b70977356b3
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-04-21 02:52:51 -0400

    Created cart product component and have shopping cart correctly showing total amount for each item

--- store/src/components/Navbar.js
+++ store/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Button, Container, Navbar, Modal, ModalBody } from "react-bootstrap";
 import { useState, useContext } from "react";
 import { CartContext } from "../CartContext";
-
+import CartProduct from "./CartProduct";
 function NavbarComponent() {
   const cart = useContext(CartContext);
   const [show, setShow] = useState(false);
@@ -32,7 +32,11 @@ function NavbarComponent() {
               <p>Items in your cart:</p>
 
               {cart.items.map((currentProduct, idx) => (
-                <h1>{currentProduct.id}</h1>
+                <CartProduct
+                  key={idx}
+                  id={currentProduct.id}
+                  quantity={currentProduct.quantity}
+                ></CartProduct>
               ))}
 
               <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
